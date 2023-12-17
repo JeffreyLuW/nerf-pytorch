39554c552708d951052333f9d0dfb72c2e7829c6
Jeffrey Lu <146838940+JeffreyLuW@users.noreply.github.com>
2023-12-17 19:33:56 +0800

    first-commit

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -10,7 +10,7 @@ img2mse = lambda x, y : torch.mean((x - y) ** 2)
 mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
 to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 
-
+#
 # Positional encoding (section 5.1)
 class Embedder:
     def __init__(self, **kwargs):
